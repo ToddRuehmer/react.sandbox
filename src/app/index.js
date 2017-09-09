52a10b9e5d3d7b5abf03c71dd1e86c641cea76e2
thiirteen <truehmer@gmail.com>
2017-09-09 14:18:15 -0500

    Theme Class Toggling

--- src/app/index.js
+++ src/app/index.js
@@ -43,13 +43,16 @@ const mathReducer = (state = {
 
 const settingsReducer = (state = {
 	theme: "light",
+	themeClass: "RE-ThemeLight",
 	lastValues: []
 }, action) => {
 	switch (action.type) {
 		case "SET_THEME":
+			var themeClass = action.payload == "light" ? "RE-ThemeLight" : "RE-ThemeDark";
 			state = {
 				...state,
 				theme: action.payload,
+				themeClass: themeClass,
 				lastValues: [...state.lastValues, action.payload]
 			};
 			break;
